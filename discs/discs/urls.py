5836c728c63710c415e8eb45b25eda0321f0997b
Justin Moehlenpah <moeh9589@bears.unco.edu>
2020-11-08 15:57:04 -0700

    minor fixes

--- discs/discs/urls.py
+++ discs/discs/urls.py
@@ -6,7 +6,7 @@ from .views import DiscEditView
 urlpatterns = [
     path('', HomeView.as_view()),
     path('home', HomeView.as_view()),
-    path('addhero/', AddDiscView.as_view(), name='disc_new'),
+    path('adddisc/', AddDiscView.as_view(), name='disc_new'),
     path('post/<int:pk>/', DiscDetailView.as_view(), name='disc_deatil'),
     path('disc', DiscView.as_view()),
     path('/edit/', DiscEditView.as_view()),
